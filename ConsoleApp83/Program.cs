a55a72adaa42de3d0fb3cea34d5a58e7cbee368a
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 15:36:19 +0330

    Add files via upload

---
+++ ConsoleApp83/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom \nto c#\nprograming!");
+            Console.ReadLine();
+        }
+    }
+}
